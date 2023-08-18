698b3c9f454f0e82301b20918d4f646e375c59ba
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-08-18 11:12:13 -0400

    removed hasClass

--- script.js
+++ script.js
@@ -9,17 +9,13 @@ controls.forEach(c => c.addEventListener('click', ()=>{
     // if the attribute tartget is the same as control
     for(let i = 0; i < myth.length; i ++){
         if(myth[i].getAttribute = cAttr){
-            if(($(myth[i]).hasClass('hidden'))){
-                myth[i].classList.remove('hidden');
-                myth[i].classList.add('fade');
-                controls[i].setAttribute('data-active', "true");
-            }
+            myth[i].classList.remove('hidden');
+            myth[i].classList.add('fade');
+            controls[i].setAttribute('data-active', "true");
         }else {
-            if(($(myth[i]).hasClass('fade'))){
-                controls[i].setAttribute('data-active', "false");
-                myth[i].classList.remove('fade');
-                myth[i].classList.add('hidden');
-            }
+            controls[i].setAttribute('data-active', "false");
+            myth[i].classList.remove('fade');
+            myth[i].classList.add('hidden');
         }
     }
     
