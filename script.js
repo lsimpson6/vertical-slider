576e1cb4b57c9a49f673440f9a2f48b2da0350e1
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-08-18 10:52:52 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+var controls = document.querySelectorAll('.control');
+var myth = docuemnt.querySelectorAll('.myths');
+
+controls.forEach(c => c.addEventListener('click', ()=>{
+    //get data target number
+    var cAttr = c.getAttribute('data-target');
+    // unhide data-myth that matches that number
+    // if the attribute tartget is the same as control
+    for(let i = 0; i < myth.count; i ++){
+        if(myth[i].getAttribute = cAttr){
+            myth[i].classList.remove('hidden');
+            myth[i].classList.add('fade');
+            controls[i].setAttribute('data-active', "true");
+        }else {
+            controls[i].setAttribute('data-active', "false");
+            myth[i].classList.remove('fade');
+            myth[i].classList.add('hidden');
+        }
+    }
+    
+}))
